1655355d2ed7db51f751c901d93e2e37e5f096be
ejpservice <ejptheservice@gmail.com>
2025-05-22 13:28:11 -0400

    numeros Ordenados de menor a mayor

---
+++ NumerosOrdenados/NumerosOrdenados/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] num = new int[10];
+int change = 0;
+
+for(int i = 0; i < num.Length; i++)
+{
+    Console.Write($"Digite el valor {i+1}: ");
+    num[i] = int.Parse( Console.ReadLine() );
+}
+
+for(int x = 0; x < num.Length-1; x++)
+{
+    for(int y = 0; y < num.Length-1; y++)
+    {
+        if (num[y] > num[y+1])
+        {
+            change = num[y];
+            num[y] = num[y + 1];
+            num[y + 1] = change;
+        }
+    }
+}
+
+
+for(int y = 0; y < num.Length-1; y++)
+{
+    Console.WriteLine($"valor de la posicion {y+1}: {num[y]}");
+}
