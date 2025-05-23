01fcfcda26c86748515644da5c2979f331df9906
ejpservice <ejptheservice@gmail.com>
2025-05-23 17:21:32 -0400

    calendario del mes actual

---
+++ CalendarioMesActual/CalendarioMesActual/Program.cs
@@ -0,0 +1,23 @@
+﻿DateTime fecha = DateTime.Now;
+
+int diaDelMes = DateTime.DaysInMonth(fecha.Year, fecha.Month);
+
+DateTime primerDiaDelMes = new DateTime(fecha.Year, fecha.Month, 1);
+
+int inicio = ((int)primerDiaDelMes.DayOfWeek + 6) % 7;
+
+Console.WriteLine("lu ma mi ju vi sa do");
+
+
+for (int i = 0; i < inicio; i++)
+{
+    Console.Write("   ");
+}
+
+for (int j = 1; j <= diaDelMes; j++)
+{
+    Console.Write($"{j,2} ");
+    if ((inicio + j) % 7 == 0)
+        Console.WriteLine();
+}
+Console.WriteLine();
