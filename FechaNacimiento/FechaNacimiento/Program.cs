e436fb6ec3589774bcaa73af341f0affee7678f0
ejpservice <ejptheservice@gmail.com>
2025-05-23 16:41:58 -0400

    dia de nacimiento

---
+++ FechaNacimiento/FechaNacimiento/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Digite su fecha de nacimiento: ");
+DateTime fechaNacimiento = Convert.ToDateTime(Console.ReadLine());
+
+Console.WriteLine($"El dia de la semana de su nacimiento es: {fechaNacimiento.DayOfWeek}");
+
+Console.ReadLine();
