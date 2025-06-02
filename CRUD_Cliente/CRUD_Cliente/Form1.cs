ac69ea86fa402103f85457783009d94f238902c9
ejpservice <ejptheservice@gmail.com>
2025-06-02 11:23:42 -0400

    CRUD CLIENTES

---
+++ CRUD_Cliente/CRUD_Cliente/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_Cliente
+{
+    public partial class Form1 : Form
+    {
+        string path = @"D:\Cursos\C# Basico ITSC\PROGRAMAS\CRUD_Cliente";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
